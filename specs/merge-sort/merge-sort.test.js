c00219125591c81392b1b8f994c0c7af546aada8
zenlex <erichrkeil@gmail.com>
2022-01-26 18:37:03 -0600

    mergesort complete

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,12 +7,30 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  if (nums.length <= 1) return nums;
+  const pivot = Math.ceil(nums.length / 2);
+  const left = nums.slice(0, pivot);
+  const right = nums.slice(pivot);
+  console.log({ nums, pivot, left, right });
+
+  return mergeArrays(mergeSort(left), mergeSort(right));
 };
 
+const mergeArrays = (leftArr = [], rightArr = []) => {
+  console.log('merging: ', { leftArr, rightArr });
+  const result = [];
+  while (leftArr.length > 0 && rightArr.length > 0) {
+    if (leftArr[0] < rightArr[0]) {
+      result.push(leftArr.shift())
+    } else {
+      result.push(rightArr.shift())
+    }
+  }
+  return result.concat(leftArr.length === 0 ? rightArr : leftArr)
+}
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
