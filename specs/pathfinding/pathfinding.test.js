a94cd4774c4329455a6cbce6c412cecf8262d36d
zenlex <erichrkeil@gmail.com>
2022-02-17 10:42:01 -0600

    pathfinding complete

--- specs/pathfinding/pathfinding.test.js
+++ specs/pathfinding/pathfinding.test.js
@@ -1,3 +1,6 @@
+const NO_ONE = 0;
+const BY_A = 1;
+const BY_B = 2;
 // write in a function thats a X by X array of arrays of numbers
 // as well two x/y combinations and have it return the shortest
 // length (you don't need to track the actual path) from point A
@@ -7,26 +10,89 @@
 // 0 – open space
 // 1 - closed space, cannot pass through. a wall
 // 2 - one of the two origination points
-//
-// you will almost certainly need to transform the maze into your own
-// data structure to keep track of all the meta data
 
-// this is a little tool I wrote to log out the maze to the console.
-// it is opinionated of how to do that and you do not have to do it
-// the way I did. however feel free to use it if you'd like
 const logMaze = require("./logger");
 
 function findShortestPathLength(maze, [xA, yA], [xB, yB]) {
-  // code goes here
+  const visited = maze.map((row, y) => row.map((origin, x) => ({
+    closed: origin === 1,
+    length: 0,
+    openedBy: NO_ONE,
+    x,
+    y
+  }))
+  );
+  visited[yA][xA].openedBy = BY_A;
+  visited[yB][xB].openedBy = BY_B;
+
+  let aQueue = [visited[yA][xA]];
+  let bQueue = [visited[yB][xB]];
+  let iteration = 0;
+
+  while (aQueue.length && bQueue.length) {
+    iteration++;
+    const aNeighbors = aQueue.reduce(
+      (acc, neighbor) => acc.concat(getNeighbors(visited, neighbor.x, neighbor.y)), []
+    );
+    aQueue = [];
+    for (let i = 0; i < aNeighbors.length; i++) {
+      const neighbor = aNeighbors[i];
+      if (neighbor.openedBy === BY_B) {
+        return neighbor.length + iteration;
+      } else if (neighbor.openedBy === NO_ONE) {
+        neighbor.length = iteration;
+        neighbor.openedBy = BY_A;
+        aQueue.push(neighbor)
+      }
+    }
+
+    const bNeighbors = bQueue.reduce(
+      (acc, neighbor) => acc.concat(getNeighbors(visited, neighbor.x, neighbor.y)), []
+    );
+    bQueue = [];
+    for (let i = 0; i < bNeighbors.length; i++) {
+      const neighbor = bNeighbors[i];
+      if (neighbor.openedBy === BY_A) {
+        return neighbor.length + iteration;
+      } else if (neighbor.openedBy === NO_ONE) {
+        neighbor.length = iteration;
+        neighbor.openedBy = BY_B;
+        bQueue.push(neighbor)
+      }
+    }
+  }
+  return -1 // no path exists
 }
 
+
+
+const getNeighbors = (visited, x, y) => {
+  const neighbors = [];
+  //[[x, y - 1], [x + 1, y], [x, y + 1], [x - 1, y]]
+  if (y - 1 >= 0 && !visited[y - 1][x].closed) {
+    neighbors.push(visited[y - 1][x])
+  }
+
+  if (x + 1 < visited[0].length && !visited[y][x + 1].closed) {
+    neighbors.push(visited[y][x + 1])
+  }
+
+  if (y + 1 < visited.length && !visited[y + 1][x].closed) {
+    neighbors.push(visited[y + 1][x])
+  }
+  if (x - 1 >= 0 && !visited[y][x - 1].closed) {
+    neighbors.push(visited[y][x - 1])
+  }
+
+  return neighbors;
+}
 // there is a visualization tool in the completed exercise
 // it requires you to shape your objects like I did
 // see the notes there if you want to use it
 
 // unit tests
 // do not modify the below code
-describe.skip("pathfinding – happy path", function () {
+describe("pathfinding – happy path", function () {
   const fourByFour = [
     [2, 0, 0, 0],
     [0, 0, 0, 0],
@@ -90,7 +156,7 @@ describe.skip("pathfinding – happy path", function () {
 // I care far less if you solve these
 // nonetheless, if you're having fun, solve some of the edge cases too!
 // just remove the .skip from describe.skip
-describe.skip("pathfinding – edge cases", function () {
+describe("pathfinding – edge cases", function () {
   const byEachOther = [
     [0, 0, 0, 0, 0],
     [0, 2, 2, 0, 0],
